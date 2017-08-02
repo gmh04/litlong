338e9f1490627dab8000f43f3a09558f6630f301
George Hamilton <gmh04@netscape.net>
2017-08-02 15:23:20 +0100

    separate creation of new edinburgh locations from assigning mentions

---
+++ scripts/add_new_edinburgh_locations.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+# https://docs.google.com/spreadsheets/d/11UDJXC5MsBCuyCQurLIZJpdNbPKGZSCC0TyWLM-yAS4/edit#gid=0
+# File -> Download as -> ods
+# Open
+# File -> Save as -> Easter egg locations and coordinates - Sheet.csv | TEXT CSV
+
+import os
+import psycopg2
+
+from os.path import expanduser
+
+db = {
+    'NAME': 'litlong',
+}
+fp = '{0}/.pgpass'.format(expanduser("~"))
+with open(fp, 'r') as pgpass:
+    for line in pgpass:
+        aline = line.split(':')
+        if aline[2] == db['NAME']:
+            db['HOST'] = aline[0]
+            db['PORT'] = aline[1]
+            db['USER'] = aline[3]
+            db['PASS'] = aline[4].strip()
+            break
+
+con = psycopg2.connect(
+   "dbname='{NAME}' user='{USER}' host='{HOST}' port={PORT} password='{PASS}'".format(**db))
+cur = con.cursor()
+
+#query = "DELETE FROM api_location WHERE id > 40726"
+#cur.execute(query)
+
+data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data')
+fname = 'Easter egg locations and coordinates.csv'
+
+with open(os.path.join(data_dir, fname), 'r') as doc:
+
+    def get_location_id(lat, lon):
+        id = None
+        query = "SELECT id FROM api_location WHERE lat = %s AND lon = %s"
+        cur.execute(query, (float(lat), float(lon)))
+        row = cur.fetchone()
+        if row != None:
+            id = row[0]
+        return id
+
+    def insert_location(text, lat, lon):
+        query = "INSERT INTO api_location(text, lat, lon, geom) values (%(text)s, %(lat)s, %(lon)s, ST_GeomFromEWKT('SRID=4326;POINT(%(lon)s %(lat)s)')) RETURNING id"
+
+        cur.execute(query, {'text': text, 'lon': float(lon), 'lat': float(lat)})
+        id = cur.fetchone()[0]
+        con.commit()
+        return id
+
+    ids = []
+    for line in doc:
+        row = line.split('|')
+        desc = row[0]
+        #print desc, desc == 'Location description '
+        if desc == 'Location description ':
+            continue
+        if desc == 'Napier Merchiston campus':
+            continue
+        if desc == 'National Gallery':
+            continue
+        coords = row[1].split(', ')
+        lat = coords[0]
+        lon = coords[1]
+        loc_id = get_location_id(lat, lon)
+        if loc_id:
+            print loc_id, desc
+            exit(0)
+        # else:
+        #     print desc
+            #exit(0)
+
+        if loc_id == None:
+            loc_id = insert_location('Edinburgh', lat, lon)
+        print loc_id, desc, lat, lon
+        ids.append(loc_id)
+    print ids
+    #exit(0)
+
+
+#con.commit()
+
+cur.close()
+con.close()
--- scripts/edinburghproblem.py
+++ scripts/edinburghproblem.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 
+# randomly assign mentions to edinburgh locations
+
 import os
 import psycopg2
 import random
 
 from os.path import expanduser
-from shutil import copyfile
-
-# https://docs.google.com/spreadsheets/d/11UDJXC5MsBCuyCQurLIZJpdNbPKGZSCC0TyWLM-yAS4/edit#gid=0
-# File -> Download as -> ods
-# Open
-# File -> Save as -> Easter egg locations and coordinates - Sheet1.csv | TEXT CSV
 
 db = {
     'NAME': 'litlong',
@@ -30,63 +26,22 @@ con = psycopg2.connect(
    "dbname='{NAME}' user='{USER}' host='{HOST}' port={PORT} password='{PASS}'".format(**db))
 cur = con.cursor()
 
-#query = "DELETE FROM api_location WHERE id > 40726"
-#cur.execute(query)
 
-data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data')
-fname = 'Easter egg locations and coordinates - Sheet1.csv'
-
-with open(os.path.join(data_dir, fname), 'r') as doc:
-
-    def get_location_id(lat, lon):
-        id = None
-        query = "SELECT id FROM api_location WHERE lat = %s AND lon = %s"
-        cur.execute(query, (float(lat), float(lon)))
-        row = cur.fetchone()
-        if row != None:
-            id = row[0]
-        return id
-
-    def insert_location(text, lat, lon):
-        query = "INSERT INTO api_location(text, lat, lon, geom) values (%(text)s, %(lat)s, %(lon)s, ST_GeomFromEWKT('SRID=4326;POINT(%(lon)s %(lat)s)')) RETURNING id"
-
-        cur.execute(query, {'text': text, 'lon': float(lon), 'lat': float(lat)})
-        id = cur.fetchone()[0]
-        con.commit()
-        return id
-
-    ids = []
-    for line in doc:
-        row = line.split('|')
-        desc = row[0]
-        #print desc, desc == 'Location description '
-        if desc == 'Location description ':
-            continue
-
-        coords = row[1].split(', ')
-        lat = coords[0]
-        lon = coords[1]
-        loc_id = get_location_id(lat, lon)
-        if loc_id:
-            print loc_id, desc
-        else:
-            print desc
-            exit(0)
-
-        if loc_id == None:
-            loc_id = insert_location('Edinburgh', lat, lon)
-        print loc_id, desc, lat, lon
-        ids.append(loc_id)
-    print ids
-    #exit(0)
-
-    query = "SELECT id FROM api_locationmention WHERE location_id IN (SELECT id FROM api_location WHERE text = 'Edinburgh')";
-    cur.execute(query)
-    for lmid in cur.fetchall():
-        query = "UPDATE api_locationmention SET location_id = %s WHERE id = %s"
-        new_loc = random.choice(ids)
-        cur.execute(query, (new_loc, lmid[0]))
-        print 'update ', lmid[0], ' with ', new_loc
+query = "SELECT id FROM api_location WHERE text = 'Edinburgh' AND id > 91";
+cur.execute(query)
+rows = cur.fetchall()
+print rows
+ids = [i[0] for i in rows]
+print ids
+print len(ids)
+#exit(0)
+query = "SELECT id FROM api_locationmention WHERE location_id IN (SELECT id FROM api_location WHERE text = 'Edinburgh')";
+cur.execute(query)
+for lmid in cur.fetchall():
+    query = "UPDATE api_locationmention SET location_id = %s WHERE id = %s"
+    new_loc = random.choice(ids)
+    cur.execute(query, (new_loc, lmid[0]))
+    print 'update ', lmid[0], ' with ', new_loc
 
 con.commit()
 
